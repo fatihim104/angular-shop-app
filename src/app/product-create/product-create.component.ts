d50b336df317416c75a322b79d0efa90ab75dfff
fatihim104 <fatihimal@gmail.com>
2023-02-19 02:40:06 +0000

    Form validation ngForm

--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Category } from '../models/category';
 import { CategoryService } from '../services/category.service';
@@ -13,8 +14,8 @@ import { ProductService } from '../services/product.service';
 export class ProductCreateComponent implements OnInit {
 
   categories:Category[]=[];
+  error:String="";
   model:any={
-    name:"Iphone 17",
     categoryId: 0
   };
 
@@ -27,11 +28,40 @@ export class ProductCreateComponent implements OnInit {
     this.categoryService.getCategories().subscribe(data => this.categories = data)
   }
 
-  saveProduct(){
-  console.log(this.model)
+  saveProduct(form:NgForm){
+
     // this.productService.createProduct(newProduct).subscribe(result => this.route.navigate(['/products']));
+    const extensions = ["jpeg", "jpg", "png"]
+    const extension = this.model.imageUrl.split(".").pop();
 
+    if(extensions.indexOf(extension) == -1){
+      this.error = "resim uzantisi sadece jpeg, jpg veya png olmalidir."
+      return;
+    }
 
+    if(this.model.categoryId == "0"){
+      this.error = "Kategori secmelisiniz";
+      return;
+    }
+
+    const product = {
+      id:null,
+      name: this.model.name,
+      price: this.model.price,
+      imageUrl: this.model.imageUrl,
+      description: this.model.description,
+      isActive: this.model.isActive,
+      categoryId: this.model.categoryId
+    }
+
+    if(form.valid){
+      this.productService.createProduct(product).subscribe(data => {
+        this.route.navigate(['/products']);
+      })
+    }else{
+      this.error = "Formu kontrol ediniz"
+    }
+   
   }
 
 }
